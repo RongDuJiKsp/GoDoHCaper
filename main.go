58fdde7a959cd7e380bf956ec183741fa90228fd
YuQiJaang <rdjksp@gmail.com>
2024-10-04 21:15:52 +0800

    add log

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"go-godoh-damon/tools"
 	"os/exec"
 	"time"
 )
 
 func run(cmd *exec.Cmd) {
+	fmt.Println("开始执行子进程")
 	_ = cmd.Run()
 }
 
 func main() {
+	fmt.Println("开始执行....")
 	nowProcess := tools.NewProcessToRun()
 	go run(nowProcess)
 	for p := range makeCommandChan() {
 		_ = nowProcess.Process.Kill()
+		fmt.Println("正在重启子进程")
 		nowProcess = p
 		go run(p)
 	}
