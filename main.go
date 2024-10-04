b6966042fa7489f1a6e3f2c734028e6246cd6bb2
YuQiJaang <rdjksp@gmail.com>
2024-10-04 21:21:35 +0800

    add 重定向

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"go-godoh-damon/tools"
+	"os"
 	"os/exec"
 	"time"
 )
 
 func run(cmd *exec.Cmd) {
 	fmt.Println("开始执行子进程")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	_ = cmd.Run()
 }
 
