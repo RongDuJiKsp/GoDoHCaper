cec58e7c571725c6012b88614ed83b25306e62dc
YuQiJaang <rdjksp@gmail.com>
2024-11-13 22:40:45 +0800

    时间更短

--- tools/new_process.go
+++ tools/new_process.go
@@ -6,7 +6,7 @@ import (
 )
 
 func NewProcessToRun() *exec.Cmd {
-	cmd := exec.Command(`godoh`, "agent", "-d", "send.tunvision.work", "-p", "cloudflare")
+	cmd := exec.Command(`godoh`, "agent", "-d", "send.tunvision.work", "-p", "cloudflare", "-t", "2")
 	fmt.Println("创建子进程成功")
 	return cmd
 }
